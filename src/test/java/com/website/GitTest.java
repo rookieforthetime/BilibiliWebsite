597f18eec8323c3f903f2db33f77fc44e9696982
dsy <dsy@study.com>
2022-09-12 12:00:23 +0800

    second commit

---
+++ src/test/java/com/website/GitTest.java
@@ -0,0 +1,12 @@
+package com.website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class GitTest {
+    @Test
+    public void gitTest(){
+        System.out.println("git 2");
+    }
+}
